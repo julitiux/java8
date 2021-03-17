852948c54044740f7f595e87e8d5b95ea3b42595
Julio Ramirez <rrodriguez.julio@gmail.com>
2021-03-16 18:02:35 -0600

    add code, for example: the tag where, replace the string list by _stringList. Add anotation Unroll. Modify the code stringList.collect by _response

--- core/src/test/groovy/mx/com/service/unitary/streams/MapServiceSpec.groovy
+++ core/src/test/groovy/mx/com/service/unitary/streams/MapServiceSpec.groovy
@@ -1,23 +1,29 @@
 package mx.com.service.unitary.streams
 
-
 import mx.com.streams.services.MapServiceImpl
 import spock.lang.Shared
 import spock.lang.Specification
+import spock.lang.Unroll
 
+@Unroll
 class MapServiceSpec extends Specification {
 
   @Shared
     service = new MapServiceImpl()
 
-  def ""() {
+  def "UNIT TEST OF METHOD STRINGTOUPPERCASE #_stringList"() {
     given:
-    List<String> stringList = ['uno', 'dos', 'tres', 'cuatro', 'cinco']
+    List<String> stringList = _stringList
     List<String> response = []
     when:
     response = service.stringToUpperCase(stringList)
     then:
-    response == stringList.collect { it.toUpperCase() }
+    response == _response
+    where:
+    _stringList                               | _response
+    []                                        | []
+    ['uno', 'dos', 'tres', 'cuatro', 'cinco'] | ['UNO', 'DOS', 'TRES', 'CUATRO', 'CINCO']
+    ['UPPERCASE']                             | ['UPPERCASE']
   }
 
 }
