63f0325a280e0e46f357c1151e8028bf594c25fd
Julio Ramirez <rrodriguez.julio@gmail.com>
2021-03-16 18:09:55 -0600

    declare method objectToString

--- api/src/main/java/mx/com/streams/services/MapService.java
+++ api/src/main/java/mx/com/streams/services/MapService.java
@@ -1,7 +1,10 @@
 package mx.com.streams.services;
 
+import mx.com.streams.domain.Staff;
+
 import java.util.List;
 
 public interface MapService {
   public List<String> stringToUpperCase(List<String> list);
+  public List<String> objectToString(List<Staff> staffList);
 }
