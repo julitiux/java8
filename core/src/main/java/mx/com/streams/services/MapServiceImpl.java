c6075da8cbd07d883e9a72a57293fac9a2c4a636
Julio Ramirez <rrodriguez.julio@gmail.com>
2021-03-16 18:10:27 -0600

    implement the method objectToString with stream and map method.

--- core/src/main/java/mx/com/streams/services/MapServiceImpl.java
+++ core/src/main/java/mx/com/streams/services/MapServiceImpl.java
@@ -1,5 +1,7 @@
 package mx.com.streams.services;
 
+import mx.com.streams.domain.Staff;
+
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -8,6 +10,11 @@ public class MapServiceImpl implements MapService {
   @Override
   public List<String> stringToUpperCase(List<String> list) {
     return list.stream().map(it -> it.toUpperCase()).collect(Collectors.toList());
+  }
+
+  @Override
+  public List<String> objectToString(List<Staff> staffList) {
+    return staffList.stream().map(it -> it.getName()).collect(Collectors.toList());
   }
 
 }
