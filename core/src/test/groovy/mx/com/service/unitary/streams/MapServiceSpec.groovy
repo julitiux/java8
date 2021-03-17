3864104ac98f203e2c663128b013c066ef495c84
Julio Ramirez <rrodriguez.julio@gmail.com>
2021-03-17 17:28:25 -0600

    implement a unit test that convert in a map with Function java

--- core/src/test/groovy/mx/com/service/unitary/streams/MapServiceSpec.groovy
+++ core/src/test/groovy/mx/com/service/unitary/streams/MapServiceSpec.groovy
@@ -1,5 +1,7 @@
 package mx.com.service.unitary.streams
 
+import mx.com.streams.domain.Staff
+import mx.com.streams.domain.StaffPublic
 import mx.com.streams.services.MapServiceImpl
 import spock.lang.Shared
 import spock.lang.Specification
@@ -24,6 +26,24 @@ class MapServiceSpec extends Specification {
     []                                        | []
     ['uno', 'dos', 'tres', 'cuatro', 'cinco'] | ['UNO', 'DOS', 'TRES', 'CUATRO', 'CINCO']
     ['UPPERCASE']                             | ['UPPERCASE']
+  }
+
+
+  def "Implement a convert in a map with Function java"() {
+    given:
+    List<Staff> staffList = _staffList
+    List<StaffPublic> response = []
+    when:
+    response = service.staffListToStaffPublicList(staffList)
+    then:
+    response*.name == _response*.name
+    where:
+    _staffList                       | _response
+    [new Staff('Julio', 36, 10000.0),
+     new Staff('Gils', 36, 11000.0),
+     new Staff('Gaio', 32, 12000.0)] | [new StaffPublic('Julio', 36, '10000'),
+                                        new StaffPublic('Gils', 36, '11000'),
+                                        new StaffPublic('Gaio', 32, '12000')]
   }
 
 }
