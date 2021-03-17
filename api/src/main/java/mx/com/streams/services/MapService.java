8911620ee465284d2c1f2c3307970de808ca7a6d
Julio Ramirez <rrodriguez.julio@gmail.com>
2021-03-17 16:26:14 -0600

    declare the method staffListToStaffPublicList

--- api/src/main/java/mx/com/streams/services/MapService.java
+++ api/src/main/java/mx/com/streams/services/MapService.java
@@ -1,10 +1,12 @@
 package mx.com.streams.services;
 
 import mx.com.streams.domain.Staff;
+import mx.com.streams.domain.StaffPublic;
 
 import java.util.List;
 
 public interface MapService {
   public List<String> stringToUpperCase(List<String> list);
   public List<String> objectToString(List<Staff> staffList);
+  public List<StaffPublic> staffListToStaffPublicList(List<Staff> staffList);
 }
