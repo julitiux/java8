d8ea3a0ae4de23345d31935b6616420c18efaf15
Julio Ramirez <rrodriguez.julio@gmail.com>
2021-03-18 16:22:43 -0600

    implement a method convertListStringStringToListString that recibe a doble list that converts in a simple list

--- core/src/main/java/mx/com/flatMap/services/FlatMapServiceImpl.java
+++ core/src/main/java/mx/com/flatMap/services/FlatMapServiceImpl.java
@@ -2,5 +2,14 @@ package mx.com.flatMap.services;
 
 import mx.com.streams.services.FlatMapService;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class FlatMapServiceImpl implements FlatMapService {
+
+  @Override
+  public List<String> convertListStringStringToListString(List<List<String>> stringListList) {
+    return stringListList.stream().map(it -> it).flatMap(it -> it.stream()).collect(Collectors.toList());
+  }
+
 }
