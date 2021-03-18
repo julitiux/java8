c7a263446c9c7209d94d8390b54c9735aea69ee7
Julio Ramirez <rrodriguez.julio@gmail.com>
2021-03-18 16:37:48 -0600

    add name of unit test

--- core/src/test/groovy/mx/com/service/unitary/streams/FlatMapServiceSpec.groovy
+++ core/src/test/groovy/mx/com/service/unitary/streams/FlatMapServiceSpec.groovy
@@ -9,7 +9,7 @@ class FlatMapServiceSpec extends Specification {
   @Shared
     service = new FlatMapServiceImpl()
 
-  def ""() {
+  def "testing flatMap in java 8"() {
     given:
     List stringListList = [[1], [2, 3, 4], [5, 6, 7], [8, 9]]
     List response = []
