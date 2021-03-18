61d7c9c0bcf742a86e4e12fe73ca278d7e46e5ba
Julio Ramirez <rrodriguez.julio@gmail.com>
2021-03-18 16:35:37 -0600

    modify the implementation, delete the map function and add lambda Collection::stream, remplaced by it -> it.stream()

--- core/src/main/java/mx/com/flatMap/services/FlatMapServiceImpl.java
+++ core/src/main/java/mx/com/flatMap/services/FlatMapServiceImpl.java
@@ -2,6 +2,7 @@ package mx.com.flatMap.services;
 
 import mx.com.streams.services.FlatMapService;
 
+import java.util.Collection;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -9,7 +10,7 @@ public class FlatMapServiceImpl implements FlatMapService {
 
   @Override
   public List<String> convertListStringStringToListString(List<List<String>> stringListList) {
-    return stringListList.stream().map(it -> it).flatMap(it -> it.stream()).collect(Collectors.toList());
+    return stringListList.stream().flatMap(Collection::stream).collect(Collectors.toList());
   }
 
 }
