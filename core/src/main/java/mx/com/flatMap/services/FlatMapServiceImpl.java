7a39942a76b81bfd979bc0397c7a1808b65f6691
Julio Ramirez <rrodriguez.julio@gmail.com>
2021-03-19 05:47:15 -0600

    implement a method that recive a user List and return a travel list using a mapFlat and method stream

--- core/src/main/java/mx/com/flatMap/services/FlatMapServiceImpl.java
+++ core/src/main/java/mx/com/flatMap/services/FlatMapServiceImpl.java
@@ -1,5 +1,7 @@
 package mx.com.flatMap.services;
 
+import mx.com.streams.domain.Travel;
+import mx.com.streams.domain.User;
 import mx.com.streams.services.FlatMapService;
 
 import java.util.Collection;
@@ -11,6 +13,11 @@ public class FlatMapServiceImpl implements FlatMapService {
   @Override
   public List<String> convertListStringStringToListString(List<List<String>> stringListList) {
     return stringListList.stream().flatMap(Collection::stream).collect(Collectors.toList());
+  }
+
+  @Override
+  public List<Travel> getTravelListByUserList(List<User> userList) {
+    return userList.stream().map(user -> user.getTravelList()).flatMap(travel -> travel.stream()).collect(Collectors.toList());
   }
 
 }
