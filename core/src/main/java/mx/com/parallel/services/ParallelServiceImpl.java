f7783d2750ca6f299bd51b2abf842c84883ce3f1
Julio Ramirez <rrodriguez.julio@gmail.com>
2021-03-19 14:19:33 -0600

    iomplement the method justPrintARangeNumberParallelOrNot using a IntStream, I delete the other one

--- core/src/main/java/mx/com/parallel/services/ParallelServiceImpl.java
+++ core/src/main/java/mx/com/parallel/services/ParallelServiceImpl.java
@@ -2,18 +2,15 @@ package mx.com.parallel.services;
 
 import mx.com.streams.services.ParallelService;
 
+import java.util.function.Consumer;
 import java.util.stream.IntStream;
 
 public class ParallelServiceImpl implements ParallelService {
 
+  Consumer<IntStream> print = System.out::println;
 
   @Override
-  public void justPrintARangeNumber(IntStream intStream) {
-    intStream.forEach(System.out::println);
-  }
-
-  @Override
-  public void justPrintARangeNumberParallel(IntStream intStream) {
-    intStream.parallel().forEach(System.out::println);
+  public void justPrintARangeNumberParallelOrNot(IntStream intStream) {
+    print.accept(intStream);
   }
 }
