1f17408a0f2708a782c782bf822adcbf87f42f0e
Julio Ramirez <rrodriguez.julio@gmail.com>
2021-03-19 05:45:32 -0600

    implement the domain Travel

---
+++ api/src/main/java/mx/com/streams/domain/Travel.java
@@ -0,0 +1,17 @@
+package mx.com.streams.domain;
+
+public class Travel {
+  String pais;
+
+  public Travel(String pais) {
+    this.pais = pais;
+  }
+
+  public String getPais() {
+    return pais;
+  }
+
+  public void setPais(String pais) {
+    this.pais = pais;
+  }
+}
