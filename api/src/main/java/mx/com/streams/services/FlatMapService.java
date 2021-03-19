e973077ee553123f42c13b7cd29323fd466bb954
Julio Ramirez <rrodriguez.julio@gmail.com>
2021-03-19 05:46:20 -0600

    declare the method getTravelListByUserList, acept a list of User and return a Travel List

--- api/src/main/java/mx/com/streams/services/FlatMapService.java
+++ api/src/main/java/mx/com/streams/services/FlatMapService.java
@@ -1,7 +1,11 @@
 package mx.com.streams.services;
 
+import mx.com.streams.domain.Travel;
+import mx.com.streams.domain.User;
+
 import java.util.List;
 
 public interface FlatMapService {
   public List<String> convertListStringStringToListString(List<List<String>> stringListList);
+  public List<Travel> getTravelListByUserList(List<User> userList);
 }
