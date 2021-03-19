409f46b5b0d96462f733d1aa0f0235bd68e01861
Julio Ramirez <rrodriguez.julio@gmail.com>
2021-03-19 14:18:45 -0600

    declare a method with another name justPrintARangeNumberParallelOrNot and delete the other one

--- api/src/main/java/mx/com/streams/services/ParallelService.java
+++ api/src/main/java/mx/com/streams/services/ParallelService.java
@@ -3,6 +3,5 @@ package mx.com.streams.services;
 import java.util.stream.IntStream;
 
 public interface ParallelService {
-  public void justPrintARangeNumber(IntStream intStream);
-  public void justPrintARangeNumberParallel(IntStream intStream);
+  public void justPrintARangeNumberParallelOrNot(IntStream intStream);
 }
