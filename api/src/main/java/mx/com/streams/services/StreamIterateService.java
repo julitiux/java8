91d113a2fe3da68a967a40cf5d75885741a406b1
Julio Ramirez <rrodriguez.julio@gmail.com>
2021-03-21 17:37:53 -0600

    declare a function fibonacci

--- api/src/main/java/mx/com/streams/services/StreamIterateService.java
+++ api/src/main/java/mx/com/streams/services/StreamIterateService.java
@@ -3,4 +3,5 @@ package mx.com.streams.services;
 public interface StreamIterateService {
   public void stream_0_9(Integer limit);
   public void streamWithFilter(Integer limit);
+  public void fibonacci(Integer limit);
 }
