91daf19b0ef7a44c06c701684430bf5d029c1e34
Julio Ramirez <rrodriguez.julio@gmail.com>
2021-03-20 21:15:33 -0600

    add the tag unroll and add the input _intStream

--- core/src/test/groovy/mx/com/service/unitary/streams/ParallelServiceSpec.groovy
+++ core/src/test/groovy/mx/com/service/unitary/streams/ParallelServiceSpec.groovy
@@ -3,9 +3,11 @@ package mx.com.service.unitary.streams
 import mx.com.parallel.services.ParallelServiceImpl
 import spock.lang.Shared
 import spock.lang.Specification
+import spock.lang.Unroll
 
 import java.util.stream.IntStream
 
+@Unroll
 class ParallelServiceSpec extends Specification {
 
   @Shared
@@ -25,7 +27,7 @@ class ParallelServiceSpec extends Specification {
     service.justPrintARangeNumberParallelOrNot(intStream.parallel())
   }
 
-  def "unit test that return if using parallel or not"() {
+  def "unit test that return if using parallel or not #_intStream"() {
     given:
     IntStream intStream = _intStream
     Boolean response
