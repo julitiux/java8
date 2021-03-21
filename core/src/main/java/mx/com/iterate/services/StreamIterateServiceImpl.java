79403357ee3df4b45444db14bd296fa3781689ad
Julio Ramirez <rrodriguez.julio@gmail.com>
2021-03-21 17:32:13 -0600

    implement a method called streamWithFilter. in this methos using Stream.iterator using a filter with a limit and sending a consumer

--- core/src/main/java/mx/com/iterate/services/StreamIterateServiceImpl.java
+++ core/src/main/java/mx/com/iterate/services/StreamIterateServiceImpl.java
@@ -10,4 +10,9 @@ public class StreamIterateServiceImpl implements StreamIterateService {
   public void stream_0_9(Integer limit) {
     Stream.iterate(0, n -> n + 1).limit(limit).forEach(System.out::println);
   }
+
+  @Override
+  public void streamWithFilter(Integer limit) {
+    Stream.iterate(0, n -> n + 1).filter(x -> x % 2 != 0).limit(limit).forEach(System.out::println);
+  }
 }
