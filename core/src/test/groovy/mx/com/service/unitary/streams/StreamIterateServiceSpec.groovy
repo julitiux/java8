9e691a64397e8d64c9d5f733cab32e2121419198
Julio Ramirez <rrodriguez.julio@gmail.com>
2021-03-21 17:44:53 -0600

    change the unit test using where and list of number _limit. Add the tag Unroll

--- core/src/test/groovy/mx/com/service/unitary/streams/StreamIterateServiceSpec.groovy
+++ core/src/test/groovy/mx/com/service/unitary/streams/StreamIterateServiceSpec.groovy
@@ -3,17 +3,21 @@ package mx.com.service.unitary.streams
 import mx.com.iterate.services.StreamIterateServiceImpl
 import spock.lang.Shared
 import spock.lang.Specification
+import spock.lang.Unroll
 
+@Unroll
 class StreamIterateServiceSpec extends Specification {
 
   @Shared
     service = new StreamIterateServiceImpl()
 
-  def ""() {
+  def "testing the method stream_0_9 with #_limit"() {
     given:
-    Integer limit = 9
+    Integer limit = _limit
     expect:
     service.stream_0_9(limit)
+    where:
+    _limit << [0, 1, 9, 1000]
   }
 
 }
