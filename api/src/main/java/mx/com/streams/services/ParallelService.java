ede2250a1e175b01d9af15ec5c84eb2db83f8071
Julio Ramirez <rrodriguez.julio@gmail.com>
2021-03-20 21:29:46 -0600

    declare a method usingParallelStrem using with a parameter Stream

--- api/src/main/java/mx/com/streams/services/ParallelService.java
+++ api/src/main/java/mx/com/streams/services/ParallelService.java
@@ -1,8 +1,11 @@
 package mx.com.streams.services;
 
+import java.util.List;
 import java.util.stream.IntStream;
+import java.util.stream.Stream;
 
 public interface ParallelService {
   public void justPrintARangeNumberParallelOrNot(IntStream intStream);
+  public void usingParallelStrem(Stream stream);
   public Boolean isParallelRunning(IntStream intStream);
 }
