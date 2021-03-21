a29e871f0353f411991e1db84bc226c5f1c34ba1
Julio Ramirez <rrodriguez.julio@gmail.com>
2021-03-21 17:38:50 -0600

    implement the method fibonacci using a limiter like paramter. this function use a Stream. iterator with a filter, map and cosuer

--- core/src/main/java/mx/com/iterate/services/StreamIterateServiceImpl.java
+++ core/src/main/java/mx/com/iterate/services/StreamIterateServiceImpl.java
@@ -15,4 +15,11 @@ public class StreamIterateServiceImpl implements StreamIterateService {
   public void streamWithFilter(Integer limit) {
     Stream.iterate(0, n -> n + 1).filter(x -> x % 2 != 0).limit(limit).forEach(System.out::println);
   }
+
+  @Override
+  public void fibonacci(Integer limit) {
+    Stream.iterate(new int[]{}, n -> new int[]{n[1], n[0] + n[1]}).limit(limit).map(n -> n[0]).forEach(System.out::println);
+  }
+
+
 }
