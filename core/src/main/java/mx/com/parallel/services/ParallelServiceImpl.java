f7d3bdc12be2c4095960db580d1450b411f22956
Julio Ramirez <rrodriguez.julio@gmail.com>
2021-03-20 21:30:38 -0600

    implement a Consumer streamConsumer that recibe a Stream. Implement the method usingParallelStrem that recibe a Stream and using the Consumer streamConsumer

--- core/src/main/java/mx/com/parallel/services/ParallelServiceImpl.java
+++ core/src/main/java/mx/com/parallel/services/ParallelServiceImpl.java
@@ -2,16 +2,24 @@ package mx.com.parallel.services;
 
 import mx.com.streams.services.ParallelService;
 
+import java.util.List;
 import java.util.function.Consumer;
 import java.util.stream.IntStream;
+import java.util.stream.Stream;
 
 public class ParallelServiceImpl implements ParallelService {
 
   Consumer<IntStream> print = it -> it.forEach(System.out::println);
+  Consumer<Stream> streamConsumer = it -> it.forEach(System.out::println);
 
   @Override
   public void justPrintARangeNumberParallelOrNot(IntStream intStream) {
     print.accept(intStream);
+  }
+
+  @Override
+  public void usingParallelStrem(Stream stream) {
+    streamConsumer.accept(stream);
   }
 
   @Override
