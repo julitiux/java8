c1d3122ad55bffd9f0c57bc746043b28bb58ea50
Julio Ramirez <rrodriguez.julio@gmail.com>
2021-03-21 22:08:01 -0600

    declare a method usingGroupBy that recibe a list of straing and return a Map with the number of repaate of each word

---
+++ api/src/main/java/mx/com/streams/services/StreamCollectService.java
@@ -0,0 +1,8 @@
+package mx.com.streams.services;
+
+import java.util.List;
+import java.util.Map;
+
+public interface StreamCollectService {
+  public Map<String, Long> usingGroupBy(List<String> stringList);
+}
