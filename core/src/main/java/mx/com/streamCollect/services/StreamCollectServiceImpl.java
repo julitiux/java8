ad3b688ea7985b3f29f4ed64a3cbd3fb60a332f1
Julio Ramirez <rrodriguez.julio@gmail.com>
2021-03-22 23:09:15 -0600

    implementn the method usingGroupByAndOrderByValue and add another things unnecesary

--- core/src/main/java/mx/com/streamCollect/services/StreamCollectServiceImpl.java
+++ core/src/main/java/mx/com/streamCollect/services/StreamCollectServiceImpl.java
@@ -2,6 +2,7 @@ package mx.com.streamCollect.services;
 
 import mx.com.streams.services.StreamCollectService;
 
+import java.util.Comparator;
 import java.util.List;
 import java.util.Map;
 import java.util.function.Function;
@@ -12,6 +13,18 @@ public class StreamCollectServiceImpl implements StreamCollectService {
   @Override
   public Map<String, Long> usingGroupBy(List<String> stringList) {
     return stringList.stream().collect(Collectors.groupingBy(Function.identity(), Collectors.counting()));
+  }
+
+  @Override
+  public Map<String, Long> usingGroupByAndOrderByValue(List<String> stringList) {
+    Map<String, Long> map = stringList.stream().collect(Collectors.groupingBy(Function.identity(), Collectors.counting()));
+    Map<String, Long> finalMap = null;
+
+    stringList.stream().sorted()
+
+
+    map.entrySet().stream().sorted(Map.Entry.<String, Long> comparingByValue().reversed()).forEachOrdered(e -> finalMap.put(e.getKey(), e.getValue()));
+    return finalMap;
   }
 
 }
