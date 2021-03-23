d2e725163eacc91c08cae51927edda64391f67d8
Julio Ramirez <rrodriguez.julio@gmail.com>
2021-03-22 23:08:19 -0600

    declare a method usingGroupByAndOrderByValue

--- api/src/main/java/mx/com/streams/services/StreamCollectService.java
+++ api/src/main/java/mx/com/streams/services/StreamCollectService.java
@@ -5,4 +5,5 @@ import java.util.Map;
 
 public interface StreamCollectService {
   public Map<String, Long> usingGroupBy(List<String> stringList);
+  public Map<String, Long> usingGroupByAndOrderByValue(List<String> stringList);
 }
