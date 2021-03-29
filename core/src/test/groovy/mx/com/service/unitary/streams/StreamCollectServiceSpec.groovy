9507b106cffa899b69094b966ac42d9d6a56794f
Julio Ramirez <rrodriguez.julio@gmail.com>
2021-03-29 17:30:24 -0600

    implement the unit test fot the method countTheKeyByRepeatInList

---
+++ core/src/test/groovy/mx/com/service/unitary/streams/StreamCollectServiceSpec.groovy
@@ -0,0 +1,33 @@
+package mx.com.service.unitary.streams
+
+import mx.com.streamCollect.services.StreamCollectServiceImpl
+import mx.com.streams.domain.Item
+import spock.lang.Shared
+import spock.lang.Specification
+
+class StreamCollectServiceSpec extends Specification {
+
+
+  @Shared
+  service = new StreamCollectServiceImpl()
+
+  def ""() {
+    given:
+    List<Item> itemList = [
+      new Item("apple", 10, new BigDecimal("9.99")),
+      new Item("banana", 20, new BigDecimal("19.99")),
+      new Item("orang", 10, new BigDecimal("29.99")),
+      new Item("watermelon", 10, new BigDecimal("29.99")),
+      new Item("papaya", 20, new BigDecimal("9.99")),
+      new Item("apple", 10, new BigDecimal("9.99")),
+      new Item("banana", 10, new BigDecimal("19.99")),
+      new Item("apple", 20, new BigDecimal("9.99"))
+    ]
+    Map response = [:]
+    when:
+    response = service.countTheKeyByRepeatInList(itemList)
+    then:
+    println response
+  }
+
+}
