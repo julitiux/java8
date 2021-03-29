c8a66b640e3a163421f262e5a2b89ab8930c9a35
Julio Ramirez <rrodriguez.julio@gmail.com>
2021-03-29 17:25:48 -0600

    declare a method countTheKeyByRepeatInList recibe a list and return a map

--- api/src/main/java/mx/com/streams/services/StreamCollectService.java
+++ api/src/main/java/mx/com/streams/services/StreamCollectService.java
@@ -1,9 +1,12 @@
 package mx.com.streams.services;
 
+import mx.com.streams.domain.Item;
+
 import java.util.List;
 import java.util.Map;
 
 public interface StreamCollectService {
   public Map<String, Long> usingGroupBy(List<String> stringList);
   public Map<String, Long> usingGroupByAndOrderByValue(List<String> stringList);
+  public Map<String, Integer> countTheKeyByRepeatInList(List<Item> itemList);
 }
