ae8b855a77c5194fb00cd57230f5197a17fcc01b
Julio Ramirez <rrodriguez.julio@gmail.com>
2021-04-07 14:33:19 -0500

    implement unit test, add more test and anotations unroll

--- core/src/test/groovy/mx/com/service/unitary/streams/FindFirstAndAnyServiceSpec.groovy
+++ core/src/test/groovy/mx/com/service/unitary/streams/FindFirstAndAnyServiceSpec.groovy
@@ -1,23 +1,29 @@
 package mx.com.service.unitary.streams
 
-import mx.com.streams.services.FindFirstAndAnyService
 import mx.com.streams.services.FindFirstAndAnyServiceImpl
 import spock.lang.Shared
 import spock.lang.Specification
+import spock.lang.Unroll
 
-class FindFirstAndAnyServiceSpec extends Specification{
+@Unroll
+class FindFirstAndAnyServiceSpec extends Specification {
 
   @Shared
-  service = new FindFirstAndAnyServiceImpl()
+    service = new FindFirstAndAnyServiceImpl()
 
-  def""(){
+  def "making unit test of find any using #_stringList"() {
     given:
-    List<String> stringList = ['one', 'two', 'three', 'four', 'five']
+    List<String> stringList = _stringList
     String response
     when:
     response = service.findAny(stringList)
     then:
-    println response
+    response == _response
+    where:
+    _stringList                             | _response
+    []                                      | 'No Value'
+    ['just one value']                      | 'just one value'
+    ['one', 'two', 'three', 'four', 'five'] | 'one'
   }
 
 }
