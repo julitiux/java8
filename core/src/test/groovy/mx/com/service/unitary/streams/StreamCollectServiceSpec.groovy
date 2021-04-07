b80560168ce182801942df789b6185c7410f4c9c
Julio Ramirez <rrodriguez.julio@gmail.com>
2021-04-06 22:14:08 -0500

    add the where apart. Modify the given apart and send all the parameter to a list _itemList

--- core/src/test/groovy/mx/com/service/unitary/streams/StreamCollectServiceSpec.groovy
+++ core/src/test/groovy/mx/com/service/unitary/streams/StreamCollectServiceSpec.groovy
@@ -12,24 +12,30 @@ class StreamCollectServiceSpec extends Specification {
 
   def ""() {
     given:
-    List<Item> itemList = [
-      new Item("apple", 10, new BigDecimal("9.99")),
-      new Item("banana", 20, new BigDecimal("19.99")),
-      new Item("orang", 10, new BigDecimal("29.99")),
-      new Item("watermelon", 10, new BigDecimal("29.99")),
-      new Item("papaya", 20, new BigDecimal("9.99")),
-      new Item("apple", 10, new BigDecimal("9.99")),
-      new Item("banana", 10, new BigDecimal("19.99")),
-      new Item("apple", 20, new BigDecimal("9.99"))
-    ]
+    List<Item> itemList = _itemList
     Map response = [:]
     when:
     response = service.countTheKeyByRepeatInList(itemList)
     then:
-    response == itemList.groupBy { it.name }.collectEntries {
+    response == _itemList.groupBy { it.name }.collectEntries {
       k, v ->
         [(k): v*.qty.sum()]
     }
+    where:
+    _itemList << [
+      [
+        new Item("apple", 10, new BigDecimal("9.99")),
+        new Item("banana", 20, new BigDecimal("19.99")),
+        new Item("orang", 10, new BigDecimal("29.99")),
+        new Item("watermelon", 10, new BigDecimal("29.99")),
+        new Item("papaya", 20, new BigDecimal("9.99")),
+        new Item("apple", 10, new BigDecimal("9.99")),
+        new Item("banana", 10, new BigDecimal("19.99")),
+        new Item("apple", 20, new BigDecimal("9.99"))
+      ]
+    ]
+
+
   }
 
 }
