7c8db57ed0652213fcedb1584ca26eec84a30387
Julio Ramirez <rrodriguez.julio@gmail.com>
2021-04-07 13:10:34 -0500

    modify by the GUI and the tutorial: I modified a the lambda by a method reference

--- core/src/main/java/mx/com/streams/services/FilterAValueNullServiceImpl.java
+++ core/src/main/java/mx/com/streams/services/FilterAValueNullServiceImpl.java
@@ -1,13 +1,14 @@
 package mx.com.streams.services;
 
 import java.util.List;
+import java.util.Objects;
 import java.util.stream.Collectors;
 
 public class FilterAValueNullServiceImpl implements FilterAValueNullService {
 
   @Override
   public List<String> filterValueNullInAList(List<String> stringList) {
-    return stringList.stream().filter(it -> it != null).collect(Collectors.toList());
+    return stringList.stream().filter(Objects::nonNull).collect(Collectors.toList());
   }
 
 }
