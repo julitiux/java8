6905cf504fd8a8fb41aaf44c078d24977650eefb
Julio Ramirez <rrodriguez.julio@gmail.com>
2021-04-08 13:06:16 -0500

    implement a better unit test, add clause where and annotation Unroll

--- core/src/test/groovy/mx/com/service/unitary/streams/StreamReduceServiceSpec.groovy
+++ core/src/test/groovy/mx/com/service/unitary/streams/StreamReduceServiceSpec.groovy
@@ -1,24 +1,28 @@
 package mx.com.service.unitary.streams
 
-
 import mx.com.streams.services.StreamReduceServiceImpl
 import spock.lang.Shared
 import spock.lang.Specification
+import spock.lang.Unroll
 
+@Unroll
 class StreamReduceServiceSpec extends Specification {
 
   @Shared
     service = new StreamReduceServiceImpl()
 
-  def ""() {
+  def "making a unit test of Stream.reduce with a reference method Integer::sum #_integerList"() {
     given:
-    List<Integer> integerList = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0]
+    List<Integer> integerList = _integerList
     Integer response
     when:
     response = service.reduceAListWithAdd integerList
     then:
-    response
-    println response
+    response.equals(_response)
+    where:
+    _integerList                         | _response
+    []                                   | 0
+    [1, 2, 3, 4, 5, 6, 7, 8, 9, 0]       | 45
   }
 
 }
