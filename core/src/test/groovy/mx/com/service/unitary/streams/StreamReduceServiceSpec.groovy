1eddd2b4dc5ffd116b0ca8798bdb3af4ff0a7ccf
Julio Ramirez <rrodriguez.julio@gmail.com>
2021-04-08 14:35:11 -0500

    making a unit test of Stream.reduce with a lambda 0, (a, b) -> a / b #_integerList

--- core/src/test/groovy/mx/com/service/unitary/streams/StreamReduceServiceSpec.groovy
+++ core/src/test/groovy/mx/com/service/unitary/streams/StreamReduceServiceSpec.groovy
@@ -40,4 +40,19 @@ class StreamReduceServiceSpec extends Specification {
     [1, 2, 3, 4, 5, 6, 7, 8, 9] | 0
   }
 
+  def "making a unit test of Stream.reduce with a lambda 0, (a, b) -> a / b #_integerList"() {
+    given:
+    List<Integer> integerList = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+    Integer response
+    when:
+    //The function multiply dont work cos, the lambda start with 0
+    response = service.reduceAListWithDivide integerList
+    then:
+    response.equals(_response)
+    where:
+    _integerList                | _response
+    []                          | 0
+    [1, 2, 3, 4, 5, 6, 7, 8, 9] | 0
+  }
+
 }
