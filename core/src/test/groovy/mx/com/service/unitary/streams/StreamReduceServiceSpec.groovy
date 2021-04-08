1e715fe0ceee7b42bbf901fec72edf02be818205
Julio Ramirez <rrodriguez.julio@gmail.com>
2021-04-08 15:27:59 -0500

    making a unit test of Stream.reduce with method reference 0, Integer::max #_integerList

--- core/src/test/groovy/mx/com/service/unitary/streams/StreamReduceServiceSpec.groovy
+++ core/src/test/groovy/mx/com/service/unitary/streams/StreamReduceServiceSpec.groovy
@@ -69,4 +69,18 @@ class StreamReduceServiceSpec extends Specification {
     [1, 2, 3, 4, 5, 6, 7, 8, 9] | -45
   }
 
+  def "making a unit test of Stream.reduce with method reference 0, Integer::max #_integerList"() {
+    given:
+    List<Integer> integerList = _integerList
+    Integer response
+    when:
+    response = service.reduceAListWithMax integerList
+    then:
+    response == _response
+    where:
+    _integerList                | _response
+    []                          | 0
+    [1, 2, 3, 4, 5, 6, 7, 8, 9] | 9
+  }
+
 }
