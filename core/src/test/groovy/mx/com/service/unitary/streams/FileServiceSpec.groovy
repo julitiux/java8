d9cd836c048d7043bd2614ea294e1d9ab50dc5b0
Julio Ramirez <rrodriguez.julio@gmail.com>
2021-04-10 15:50:17 -0500

    modify unit test with Stream where filename #_filename

--- core/src/test/groovy/mx/com/service/unitary/streams/FileServiceSpec.groovy
+++ core/src/test/groovy/mx/com/service/unitary/streams/FileServiceSpec.groovy
@@ -3,17 +3,21 @@ package mx.com.service.unitary.streams
 import mx.com.streams.services.FileServiceImpl
 import spock.lang.Shared
 import spock.lang.Specification
+import spock.lang.Unroll
 
+@Unroll
 class FileServiceSpec extends Specification {
 
   @Shared
     service = new FileServiceImpl();
 
-  def ""() {
+  def "making unit test with Stream where filename #_filename"() {
     given:
-    String filename = "src/main/resources/Files/lines.txt"
+    String filename = _filename
     expect:
     service.readFileLineByLine(filename)
+    where:
+    _filename << ["src/main/resources/Files/lines.txt"]
   }
 
 }
