b013801f4e0964b8104bdc1205e70262e10dd8d8
Julio Ramirez <rrodriguez.julio@gmail.com>
2021-04-10 15:52:58 -0500

    modify the name of method readFileLineByLineFilterLines adding parameter startWith

--- api/src/main/java/mx/com/streams/services/FileService.java
+++ api/src/main/java/mx/com/streams/services/FileService.java
@@ -2,5 +2,5 @@ package mx.com.streams.services;
 
 public interface FileService {
   void readFileLineByLine(String filename);
-  void readFileLineByLineFilterLines(String filename);
+  void readFileLineByLineFilterLines(String filename, String startWith);
 }
