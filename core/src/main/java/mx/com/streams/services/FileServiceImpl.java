6fa6ffe4aaedda788b01b86d03c336c774747fcd
Julio Ramirez <rrodriguez.julio@gmail.com>
2021-04-10 15:53:18 -0500

    modify the methos readFileLineByLineFilterLines adding the parameter startWith

--- core/src/main/java/mx/com/streams/services/FileServiceImpl.java
+++ core/src/main/java/mx/com/streams/services/FileServiceImpl.java
@@ -17,9 +17,9 @@ public class FileServiceImpl implements FileService {
   }
 
   @Override
-  public void readFileLineByLineFilterLines(String filename) {
+  public void readFileLineByLineFilterLines(String filename, String startWith) {
     try (Stream<String> stream = Files.lines(Paths.get(filename))) {
-      stream.filter(line -> line.startsWith("linea")).map(String::toUpperCase).forEach(System.out::println);
+      stream.filter(line -> line.startsWith(startWith)).map(String::toUpperCase).forEach(System.out::println);
     } catch (IOException e) {
       e.printStackTrace();
     }
