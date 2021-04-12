8fafcb7f179f49638c8ad320a5fe3787d433e33d
Julio Ramirez <rrodriguez.julio@gmail.com>
2021-04-12 16:57:56 -0500

    declare and implement a method called convertStreamToArrayInt that recive two number startRange and endRange

--- api/src/main/java/mx/com/streams/services/StreamToArrayService.java
+++ api/src/main/java/mx/com/streams/services/StreamToArrayService.java
@@ -3,4 +3,5 @@ package mx.com.streams.services;
 public interface StreamToArrayService {
   String[] convertStringToArray(String string);
   Integer[] convertArrayIntToArrayInteger(int[] integers);
+  int[] convertStreamToArrayInt(Integer startRange, Integer endRange);
 }
--- core/src/main/java/mx/com/streams/services/StreamToArrayServiceImpl.java
+++ core/src/main/java/mx/com/streams/services/StreamToArrayServiceImpl.java
@@ -1,6 +1,7 @@
 package mx.com.streams.services;
 
 import java.util.Arrays;
+import java.util.stream.IntStream;
 
 public class StreamToArrayServiceImpl implements StreamToArrayService {
 
@@ -12,6 +13,11 @@ public class StreamToArrayServiceImpl implements StreamToArrayService {
   @Override
   public Integer[] convertArrayIntToArrayInteger(int[] integers) {
     return Arrays.stream(integers).map(integer -> integer * 2).boxed().toArray(Integer[]::new);
+  }
+
+  @Override
+  public int[] convertStreamToArrayInt(Integer startRange, Integer endRange) {
+    return IntStream.rangeClosed(startRange, endRange).toArray();
   }
 
 }
