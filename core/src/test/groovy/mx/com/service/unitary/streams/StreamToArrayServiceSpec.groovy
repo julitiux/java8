5bf596cccd4a5f3897a6ac193b80dae62dedc362
Julio Ramirez <rrodriguez.julio@gmail.com>
2021-04-12 16:00:24 -0500

    making a unit test using Arrray.Stream(string.split('\s+')), just for transform a String to Array

---
+++ core/src/test/groovy/mx/com/service/unitary/streams/StreamToArrayServiceSpec.groovy
@@ -0,0 +1,25 @@
+package mx.com.service.unitary.streams
+
+
+import mx.com.streams.services.StreamToArrayServiceImpl
+import spock.lang.Shared
+import spock.lang.Specification
+import spock.lang.Unroll
+
+@Unroll
+class StreamToArrayServiceSpec extends Specification {
+
+  @Shared
+    service = new StreamToArrayServiceImpl()
+
+  def ""() {
+    given:
+    String string = "this is a important test"
+    String[] response
+    when:
+    response = service.getStreamToArray(string)
+    then:
+    response.each {println it}
+  }
+
+}
