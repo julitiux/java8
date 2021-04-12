11b5a4358bcc30ec6c949f4b41ea62306cd1e338
Julio Ramirez <rrodriguez.julio@gmail.com>
2021-04-12 12:52:16 -0500

    declare and implement a method that use peek and count

---
+++ api/src/main/java/mx/com/streams/services/PeekService.java
@@ -0,0 +1,9 @@
+package mx.com.streams.services;
+
+import java.util.List;
+
+public interface PeekService {
+
+  void testPeekFirst(List<String> stringList);
+
+}
---
+++ core/src/main/java/mx/com/streams/services/PeekServiceImpl.java
@@ -0,0 +1,12 @@
+package mx.com.streams.services;
+
+import java.util.List;
+
+public class PeekServiceImpl implements PeekService {
+
+  @Override
+  public void testPeekFirst(List<String> stringList) {
+    stringList.stream().peek(System.out::println);
+  }
+
+}
