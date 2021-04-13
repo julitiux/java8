1fa52d072262cb933f8447ef3546fa38231a9de1
Julio Ramirez <rrodriguez.julio@gmail.com>
2021-04-13 16:20:49 -0500

    declare and implement a method that used a method of stream; with sorted and Comparator.comparing

--- api/src/main/java/mx/com/streams/services/StreamSortedService.java
+++ api/src/main/java/mx/com/streams/services/StreamSortedService.java
@@ -1,8 +1,11 @@
 package mx.com.streams.services;
 
+import mx.com.streams.domain.Person;
+
 import java.util.List;
 
 public interface StreamSortedService {
   List<String> sortedNatural(List<String> stringList);
   List<String> sortedWithReverseOrder(List<String> stringList);
+  List<Person> sortedPersonByUsername(List<Person> personList);
 }
--- core/src/main/java/mx/com/streams/services/StreamSortedServiceImpl.java
+++ core/src/main/java/mx/com/streams/services/StreamSortedServiceImpl.java
@@ -1,5 +1,7 @@
 package mx.com.streams.services;
 
+import mx.com.streams.domain.Person;
+
 import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -14,6 +16,11 @@ public class StreamSortedServiceImpl implements StreamSortedService{
   @Override
   public List<String> sortedWithReverseOrder(List<String> stringList) {
     return stringList.stream().sorted(Comparator.reverseOrder()).collect(Collectors.toList());
+  }
+
+  @Override
+  public List<Person> sortedPersonByUsername(List<Person> personList) {
+    return personList.stream().sorted(Comparator.comparing(Person::getUsername)).collect(Collectors.toList());
   }
 
 }
